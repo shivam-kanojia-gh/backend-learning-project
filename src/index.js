ef39b8c734ed59b7f03a9b41ab67b64241ffd2a4
Shivam Kanojia <shivamkanojia.works@gmail.com>
2025-04-15 17:30:28 +0530

    connected to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config();
+
+connectDB();
+
+// const app = express();
+
+// (async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URI);
+//     app.on("error", (error) => {
+//       console.log("ERR: ", error);
+//       throw error;
+//     });
+//     app.listen(process.env.PORT, () => {
+//       console.log(`App is listening on port ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.error("ERROR: ", error);
+//     throw error;
+//   }
+// })();
